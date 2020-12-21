40e1cebbb8c67181fd595b5921c4d071c0b9c0d4
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-12-20 22:44:43 -0600

    Major: Support Multiple Instances of Cust. El.

--- js/modules/importHTML.js
+++ js/modules/importHTML.js
@@ -1,23 +1,17 @@
 /**
  * Clone markup from an endpoint into a specific element
- * @param {HTMLElement} containerElement - The element in which to place the markup
- * @param {string} markupUrl - The URL at which to fetch the markup
- * @param {function} [callback] - A function to run after placing the markup
- * @todo Rewrite function to be a custom element
-*/
-export function fromURL(containerElement, markupUrl, callback) {
+ * @param {tring} markupUrl - The URL at which to fetch the markup
+ * @returns {Promise<string>} Promise that returns markup if resolved
+ */
+export function fromURL(markupUrl) {
   if (markupUrl) {
-    fetch(markupUrl).then((response) => {
-      return response.text();
-    }).then((data) => {
-      containerElement.innerHTML = data;
-    }, (err) => {
-      containerElement.innerHTML = null;
+    return fetch(markupUrl).then((response) => {
+      const markup = response.text();
+      return markup;
+    }).catch(err => {
       console.error(err);
-    }).finally(() => {
-      if (typeof callback === 'function') {
-        callback();
-      }
     });
+  } else {
+    return Promise.reject(new Error('No `markupUrl` provided'));
   }
 }
