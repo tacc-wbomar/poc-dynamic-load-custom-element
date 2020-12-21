d576355428cbdae2cf6724b5554f9b2fa6dc11bc
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-12-20 23:37:14 -0600

    Minor: Support two ways to importHTML
    (and renamed `fromURL` to `getFromURL`)

--- js/modules/importHTML.js
+++ js/modules/importHTML.js
@@ -1,17 +1,34 @@
 /**
- * Clone markup from an endpoint into a specific element
- * @param {tring} markupUrl - The URL at which to fetch the markup
+ * Return markup from an endpoint
+ * @param {string} markupURL - The URL from which to get the markup
  * @returns {Promise<string>} Promise that returns markup if resolved
  */
-export function fromURL(markupUrl) {
-  if (markupUrl) {
-    return fetch(markupUrl).then((response) => {
+export function getFromURL(markupURL) {
+  if (markupURL) {
+    return fetch(markupURL).then(response => {
       const markup = response.text();
       return markup;
     }).catch(err => {
       console.error(err);
     });
   } else {
-    return Promise.reject(new Error('No `markupUrl` provided'));
+    return Promise.reject(new Error('Invalid `markupURL` provided'));
+  }
+}
+
+/**
+ * Insert markup from an endpoint into a specific element
+ * @param {string} markupURL - The URL from which to get the markup
+ * @param {HTMLElement} container - The element into which to place the markup
+ * @returns {Promise<HTMLElement>} Promise that returns the container
+ */
+export function insertFromURL(markupURL, container) {
+  if (markupURL) {
+    return getFromURL(markupURL).then(markup => {
+      container.innerHTML = markup;
+      return container;
+    });
+  } else {
+    return Promise.reject(new Error('Invalid `container` provided'));
   }
 }
